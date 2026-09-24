f9f9bed87f54f0e7adbdd95ea198e1e549c32427
Jose Gambin <josemanuel.gambin@gmail.com>
2026-09-24 19:30:39 +0200

    add spring boot

---
+++ backend-java/E-Cargo-Hub/src/test/java/E_Cargo/Hub/ECargoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package E_Cargo.Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECargoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
